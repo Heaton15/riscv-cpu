cda21e5d1f8079fe332179910665500e3b53ba26
Tim <Heaton15@gmail.com>
2025-11-06 15:40:23 -0600

    Redirect all output from bender outpust to /dev/null to clean up stdout

--- scripts/harden.sh
+++ scripts/harden.sh
@@ -5,6 +5,8 @@ PACKAGES=$(bender packages)
 
 for package in "${PACKAGES[@]}"; do
   echo "Initializing Package: $package"
-  bender clone -p ./ "$package"
-  pushd . && cd "$package" && git fetch && git checkout main
+  bender clone -p ./ "$package" > /dev/null
+
+  # This assumes everything is main, so we'll need to change that later
+  (pushd . && cd "$package" && git fetch && git checkout main) > /dev/null 2>&1
 done
